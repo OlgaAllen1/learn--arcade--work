979d99a61ebfe59995c4976faf89c8b4847485bb
Olga Allen <32139467+OlgaAllen1@users.noreply.github.com>
2023-01-18 20:13:15 -0800

    first commit

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,10 @@
+print("""Praise the wet snow
+        falling early.
+Praise the shadow
+        my neighbor's chimney casts on the tile roof
+even this gray October day that should, they say,
+have been golden.""")
+
+print("This\nis\nmy\nsample.")
+
+print("I\t love\t to study\n Python")
