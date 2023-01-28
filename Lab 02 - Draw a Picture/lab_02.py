f1bc4326afce506fb75cedf701323286a682c46a
Olga Allen <32139467+OlgaAllen1@users.noreply.github.com>
2023-01-27 22:03:08 -0800

    Update lab_02.py

--- Lab 02 - Draw a Picture/lab_02.py
+++ Lab 02 - Draw a Picture/lab_02.py
@@ -10,7 +10,7 @@ import arcade
 arcade.open_window(600, 600, "My first picture")
 
 # Set the background color
-arcade.set_background_color(arcade.csscolor.SKY_BLUE)
+arcade.set_background_color(arcade.csscolor.LIGHT_BLUE)
 
 # Get ready to draw
 arcade.start_render()
@@ -18,58 +18,63 @@ arcade.start_render()
 # Draw a rectangle
 # Left of 0, right of 599
 # Top of 300, bottom of 0
-arcade.draw_lrtb_rectangle_filled(0, 599, 300, 0, arcade.csscolor.GREEN)
+arcade.draw_lrtb_rectangle_filled(0, 599, 300, 0, (227, 227, 227))
 
-# Tree trunk
-arcade.draw_rectangle_filled(100, 320, 20, 60, arcade.csscolor.SIENNA)
+# House
+arcade.draw_triangle_filled(140, 450, 300, 560, 460, 450, arcade.csscolor.BLACK)
+arcade.draw_polygon_filled([
+    [150, 300],
+    [150, 450],
+    [300, 550],
+    [450, 450],
+    [450, 300]
+], arcade.csscolor.SADDLE_BROWN)
+arcade.draw_rectangle_filled(220, 370, 80, 140, arcade.csscolor.BLACK)
+arcade.draw_rectangle_filled(220, 370, 75, 135, arcade.csscolor.BROWN)
+arcade.draw_rectangle_filled(355, 380, 140, 110, arcade.csscolor.BLACK)
+arcade.draw_rectangle_filled(355, 380, 135, 105, arcade.csscolor.DARK_GRAY)
+arcade.draw_rectangle_filled(355, 380, 125, 95, arcade.csscolor.BLACK)
 
-# Tree top
-arcade.draw_circle_filled(100, 350, 30, arcade.csscolor.DARK_GREEN)
+# Snowman
+arcade.draw_circle_filled(100, 350, 20, arcade.csscolor.GHOST_WHITE)
+arcade.draw_circle_filled(100, 308, 30, arcade.csscolor.GHOST_WHITE)
+arcade.draw_circle_filled(100, 250, 40, arcade.csscolor.GHOST_WHITE)
+arcade.draw_circle_filled(92, 355, 4, arcade.csscolor.BLACK)
+arcade.draw_circle_filled(108, 355, 4, arcade.csscolor.BLACK)
+arcade.draw_triangle_filled(100, 350, 130, 345, 100, 340, arcade.csscolor.ORANGE)
 
-# Another tree, with a trunk and ellipse for top
-arcade.draw_rectangle_filled(200, 320, 20, 60, arcade.csscolor.SIENNA)
-arcade.draw_ellipse_filled(200, 370, 60, 80, arcade.csscolor.DARK_GREEN)
-
-# Another tree, with a trunk and arc for top
-# Arc is centered at (300, 340) with a width of 60 and height of 100.
-# The starting angle is 0, and ending angle is 180.
-arcade.draw_rectangle_filled(300, 320, 20, 60, arcade.csscolor.SIENNA)
-arcade.draw_arc_filled(300, 340, 60, 100, arcade.csscolor.DARK_GREEN, 0, 180)
-
-# Another tree, with a trunk and triangle for top
+# Christmas tree, with a trunk and triangle for top
 # Triangle is made of these three points:
-# (400, 400), (370, 320), (430, 320)
+# (400, 470), (350, 320), (450, 320)
 arcade.draw_rectangle_filled(400, 320, 20, 60, arcade.csscolor.SIENNA)
-arcade.draw_triangle_filled(400, 400, 370, 320, 430, 320, arcade.csscolor.DARK_GREEN)
-
-# Draw a tree using a polygon with a list of points
-arcade.draw_rectangle_filled(500, 320, 20, 60, arcade.csscolor.SIENNA)
-arcade.draw_polygon_filled(((500, 400),
-                            (480, 360),
-                            (470, 320),
-                            (530, 320),
-                            (520, 360)
-                            ),
-                           arcade.csscolor.DARK_GREEN)
+arcade.draw_triangle_filled(400, 470, 350, 320, 450, 320, arcade.csscolor.DARK_GREEN)
+arcade.draw_triangle_filled(400, 480, 390, 470, 410, 470, arcade.csscolor.RED)
+arcade.draw_circle_filled(390, 440, 5, arcade.csscolor.ORANGE)
+arcade.draw_circle_filled(400, 420, 5, arcade.csscolor.PINK)
+arcade.draw_circle_filled(420, 400, 5, arcade.csscolor.PURPLE)
+arcade.draw_circle_filled(410, 385, 5, arcade.csscolor.BLUE)
+arcade.draw_circle_filled(395, 365, 5, arcade.csscolor.ORANGE)
+arcade.draw_circle_filled(378, 345, 5, arcade.csscolor.RED)
+arcade.draw_circle_filled(358, 345, 5, arcade.csscolor.LIME)
 
 # Draw a sun
-arcade.draw_circle_filled(500, 550, 40, arcade.color.YELLOW)
+arcade.draw_circle_filled(100, 550, 40, arcade.color.YELLOW)
 
 # Rays to the left, right, up, and down
-arcade.draw_line(500, 550, 400, 550, arcade.color.YELLOW, 3)
-arcade.draw_line(500, 550, 600, 550, arcade.color.YELLOW, 3)
-arcade.draw_line(500, 550, 500, 450, arcade.color.YELLOW, 3)
-arcade.draw_line(500, 550, 500, 650, arcade.color.YELLOW, 3)
+arcade.draw_line(100, 550, 100, 600, arcade.color.YELLOW, 3)
+arcade.draw_line(100, 550, 0, 550, arcade.color.YELLOW, 3)
+arcade.draw_line(100, 550, 0, 450, arcade.color.YELLOW, 3)
+arcade.draw_line(100, 550, 0, 650, arcade.color.YELLOW, 3)
 
 # Diagonal rays
-arcade.draw_line(500, 550, 550, 600, arcade.color.YELLOW, 3)
-arcade.draw_line(500, 550, 550, 500, arcade.color.YELLOW, 3)
-arcade.draw_line(500, 550, 450, 600, arcade.color.YELLOW, 3)
-arcade.draw_line(500, 550, 450, 500, arcade.color.YELLOW, 3)
+arcade.draw_line(100, 550, 200, 450, arcade.color.YELLOW, 3)
+arcade.draw_line(100, 550, 100, 450, arcade.color.YELLOW, 3)
+arcade.draw_line(100, 550, 150, 600, arcade.color.YELLOW, 3)
+arcade.draw_line(100, 550, 200, 550, arcade.color.YELLOW, 3)
 
-# Draw text at (150, 230) with a font size of 24 pts.
-arcade.draw_text("Arbor Day - Plant a Tree!",
-                 150, 230,
+# Draw text with a font size of 24 pts.
+arcade.draw_text("Merry Christmas and Happy New Year!",
+                 15, 150,
                  arcade.color.BLACK, 24)
 
 # Finish drawing
