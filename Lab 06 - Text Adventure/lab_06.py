6c36de373d1c94737f73c801b4a1d9969a3adcc4
Olga Allen <32139467+OlgaAllen1@users.noreply.github.com>
2023-02-16 18:53:55 -0700

    second version of Adventure game

--- Lab 06 - Text Adventure/lab_06.py
+++ Lab 06 - Text Adventure/lab_06.py
@@ -10,19 +10,37 @@ class Room:
 def main():
 
     room_list = []
-    room = Room("Bedroom 2", 3, 1, None, None)
+    room = Room("You are in the guest bedroom. "
+                "There is a room to the North and the South Hall is to the East."
+                "What direction?"
+                , 3, 1, None, None)
     room_list.append(room)
-    room = Room("South Hall", 4, 2, None, 0)
+    room = Room("You are in the South Hall."
+                " You can move to the North, East and West."
+                " What direction?"
+                , 4, 2, None, 0)
     room_list.append(room)
-    room = Room("Dining Room", 5, None, None, 1)
+    room = Room("You are in the Dining Room."
+                " The kitchen is up North and you can go west to reach South Hall."
+                " What direction?"
+                , 5, None, None, 1)
     room_list.append(room)
-    room = Room("Bedroom 1", None, 4, 0, None)
+    room = Room("You are in the Master Bedroom. "
+                "You can to to North Hall by heading East and the guest bedroom is to the south."
+                "What direction?"
+                , None, 4, 0, None)
     room_list.append(room)
-    room = Room("North Hall", 6, 5, 1, 3)
+    room = Room("You are in the torch-lit hallway. "
+                "You can go all 4 directions."
+                "What direction?", 6, 5, 1, 3)
     room_list.append(room)
-    room = Room("Kitchen", None, None, 2, 4)
+    room = Room("You are in the kitchen. "
+                "You can go West or South."
+                "What direction? ", None, None, 2, 4)
     room_list.append(room)
-    room = Room("Balcony", None, None, 4, None)
+    room = Room("You are on the Balcony."
+                " You can only go South."
+                "What direction?", None, None, 4, None)
     room_list.append(room)
 
     current_room = 1
