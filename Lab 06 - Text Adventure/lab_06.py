c3c21ee862f8a91717dbceb986396f1afb8d9236
Olga Allen <32139467+OlgaAllen1@users.noreply.github.com>
2023-02-17 20:59:02 -0700

    final version of lab 6

--- Lab 06 - Text Adventure/lab_06.py
+++ Lab 06 - Text Adventure/lab_06.py
@@ -10,44 +10,77 @@ class Room:
 def main():
 
     room_list = []
-    room = Room("You are in the guest bedroom. "
+    room = Room("You are in the guest bedroom. \n"
                 "There is a room to the North and the South Hall is to the East."
-                "What direction?"
                 , 3, 1, None, None)
     room_list.append(room)
-    room = Room("You are in the South Hall."
+    room = Room("You are in the South Hall. \n"
                 " You can move to the North, East and West."
-                " What direction?"
                 , 4, 2, None, 0)
     room_list.append(room)
-    room = Room("You are in the Dining Room."
+    room = Room("You are in the Dining Room. \n"
                 " The kitchen is up North and you can go west to reach South Hall."
-                " What direction?"
                 , 5, None, None, 1)
     room_list.append(room)
-    room = Room("You are in the Master Bedroom. "
+    room = Room("You are in the Master Bedroom. \n"
                 "You can to to North Hall by heading East and the guest bedroom is to the south."
-                "What direction?"
                 , None, 4, 0, None)
     room_list.append(room)
-    room = Room("You are in the torch-lit hallway. "
+    room = Room("You are in the torch-lit hallway. \n "
                 "You can go all 4 directions."
-                "What direction?", 6, 5, 1, 3)
+                , 6, 5, 1, 3)
     room_list.append(room)
-    room = Room("You are in the kitchen. "
+    room = Room("You are in the kitchen. \n "
                 "You can go West or South."
-                "What direction? ", None, None, 2, 4)
+                , None, None, 2, 4)
     room_list.append(room)
-    room = Room("You are on the Balcony."
+    room = Room("You are on the Balcony. \n"
                 " You can only go South."
-                "What direction?", None, None, 4, None)
+                , None, None, 4, None)
     room_list.append(room)
 
-    current_room = 1
-    print(room_list)
-    print(room_list[current_room].description)
+    current_room = 0
+
     done = False
+    while not done:
+        print()
+        print(room_list[current_room].description)
+        answer = input("What direction? (Q is for quit) ").upper()
 
+        # North
+        if answer == 'N' or answer == "NORTH":
+            next_room = room_list[current_room].north
+            if next_room is None:
+                print("There is no room on the North.")
+            else:
+                current_room = next_room
 
+        # East
+        elif answer == 'E' or answer == "EAST":
+            next_room = room_list[current_room].east
+            if next_room is None:
+                print("There is no room on the East.")
+            else:
+                current_room = next_room
+
+        # South
+        elif answer == 'S' or answer == "SOUTH":
+            next_room = room_list[current_room].south
+            if next_room is None:
+                print("There is no room on the South.")
+            else:
+                current_room = next_room
+
+        # West
+        elif answer == 'W' or answer == "WEST":
+            next_room = room_list[current_room].west
+            if next_room is None:
+                print("There is no room on the West.")
+            else:
+                current_room = next_room
+
+        elif answer == "Q":
+            print("The end")
+            done = True
 main()
 
