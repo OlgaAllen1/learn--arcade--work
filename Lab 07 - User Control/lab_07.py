0dbf0504affe04f1045fdef444f6f8086f20859e
Olga Allen <32139467+OlgaAllen1@users.noreply.github.com>
2023-03-21 18:27:05 -0600

    added comments

--- Lab 07 - User Control/lab_07.py
+++ Lab 07 - User Control/lab_07.py
@@ -7,7 +7,15 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SNOWMAN_SPEED = 3
 
+
 class SnowMan:
+    """
+    Building Snowman
+    The center for the snowman is the center of the top circle.
+    Attributes:
+        change-the speed of the snowman
+    """
+
     def __init__(self):
         self.center_x = 100
         self.change_x = 0
@@ -25,6 +33,9 @@ class SnowMan:
                                     self.center_y-10, arcade.csscolor.ORANGE)
 
     def update(self):
+        """
+        The logic for hitting the end of the screen on the right and on the left, up and down
+        """
         self.center_x = self.center_x + self.change_x
         if self.center_x > SCREEN_WIDTH - 40 or self.center_x < 40:
             self.change_x = -self.change_x
@@ -33,26 +44,31 @@ class SnowMan:
         self.center_y = self.center_y + self.change_y
         if self.center_y > SCREEN_HEIGHT/2 + 130 or self.center_y < 140:
             self.change_y = -self.change_y
-            arcade.play_sound(self.sound)
+            arcade.play_sound(self.sound)  # playing "Boing" sound
 
 
 class ChristmasTree:
+    """
+    Building Christmas Tree
+    The center for the Christmas Tree is the center of the top triangle.
+    """
 
     def __init__(self):
         self.center_x = 400
         self.center_y = 470
-        self.grabbed = False
 
     def draw(self):
-        # Christmas tree, with a trunk and triangle for top
-        # Triangle is made of these three points:
-        # (400, 470), (350, 320), (450, 320)
-        # (self.center-x, self-center_y), (Self
+        # trunc of the tree
         arcade.draw_rectangle_filled(self.center_x, self.center_y-150, 20, 60, arcade.csscolor.SIENNA)
+
+        # main triangle
         arcade.draw_triangle_filled(self.center_x, self.center_y, self.center_x-50, self.center_y-150, self.center_x+50,
                                     self.center_y-150, arcade.csscolor.DARK_GREEN)
+        # top triangle "star"
         arcade.draw_triangle_filled(self.center_x, self.center_y+10, self.center_x-10, self.center_y, self.center_x+10,
                                     self.center_y, arcade.csscolor.RED)
+
+        # Christmas Decoration for the tree
         # arcade.draw_circle_filled(390, 440, 5, arcade.csscolor.ORANGE)
         # arcade.draw_circle_filled(400, 420, 5, arcade.csscolor.PINK)
         # arcade.draw_circle_filled(420, 400, 5, arcade.csscolor.PURPLE)
@@ -63,7 +79,11 @@ class ChristmasTree:
 
 
 class MyGame(arcade.Window):
-    """ Our Custom Window Class"""
+    """
+     Our Custom Window Class
+     Attributes:
+         clicker-logical variable that corresponds to clicking mouse with left button
+    """
 
     def __init__(self):
         """ Initializer """
@@ -72,16 +92,15 @@ class MyGame(arcade.Window):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Lab 7 - User Control")
         self.snowman = SnowMan()
         self.christmas_tree = ChristmasTree()
+        self.clicker = False
 
     def on_draw(self):
         arcade.start_render()
         # Set the background color
         arcade.set_background_color(arcade.csscolor.LIGHT_BLUE)
 
-        # Draw a rectangle
-        # Left of 0, right of 599
-        # Top of 300, bottom of 0
-        arcade.draw_lrtb_rectangle_filled(0, SCREEN_WIDTH, 300, 0, (227, 227, 227))
+        # Draw snow
+        arcade.draw_lrtb_rectangle_filled(0, SCREEN_WIDTH, SCREEN_HEIGHT/2, 0, (227, 227, 227))
 
         # House
         arcade.draw_triangle_filled(140, 450, 300, 560, 460, 450, arcade.csscolor.BLACK)
@@ -137,13 +156,13 @@ class MyGame(arcade.Window):
             self.snowman.change_y = 0
 
     def on_mouse_motion(self, x, y, dx, dy):
-        if 180 < y < SCREEN_HEIGHT * 3/4 + 30 and 50 < x < SCREEN_WIDTH-50:
+        if 180 < y < SCREEN_HEIGHT * 3/4 + 30 and 50 < x < SCREEN_WIDTH-50 and self.clicker:
             self.christmas_tree.center_x = x
             self.christmas_tree.center_y = y
 
     def on_mouse_press(self, x, y, button, modifiers):
         if button == arcade.MOUSE_BUTTON_LEFT:
-            pass
+            self.clicker = not self.clicker
 
 
 def main():
