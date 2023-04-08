9ab8eedbe5f9810dbc4fba879100a8c607fa5b58
Olga Allen <32139467+OlgaAllen1@users.noreply.github.com>
2023-04-07 23:37:21 -0600

    First commit

--- Lab 10 - Spell Check/lab_10.py
+++ Lab 10 - Spell Check/lab_10.py
@@ -10,13 +10,15 @@ def split_line(line):
 
 
 dictionary_list = []  # list of all the words in the dictionary
-dictionary_file = open("dictionary.txt")  # to read
-for line in dictionary_file:
-    line = line.strip()
-    dictionary_list.append(line)
+dictionary_file = open("dictionary.txt")  # Opening file dictionary.txt to read
+for line in dictionary_file:  # for each line in dictionary.txt (for each word)
+    for line in dictionary_file:  # for each line in dictionary.txt (for each word)
+        dictionary_list.append(line.strip()) # add word into the dictionary list,
+        # and remove ant line feeds or spaces (.strip)
+dictionary_file.close() # Close file after operation done
 
-dictionary_file.close()
 
+# Linear Search Version
 print("---Linear Search---")
 file = open("AliceInWonderLand200.txt")
 for number, line in enumerate (file):
@@ -29,29 +31,39 @@ for number, line in enumerate (file):
         # current position is equal to the word
         while current_list_position < len(dictionary_list) and dictionary_list[current_list_position] != word.upper():
             # Advance to the next item in the list
-            current_list_position += 1
+            current_list_position += 1  # Advance to the next word in the dictionary
 
-        if current_list_position == len(dictionary_list):
+        if current_list_position == len(dictionary_list):  # If iterated through
+            # all words in dictionary, and not founded i
             print(f"Line {number + 1}, possible misspelled word:{word}")
 
-file.close()
+file.close()  # Closing the AliceInWonderLand200.txt
 
+# Binary Search Version
 print("---Binary Search---")
-file = open("AliceInWonderLand200.txt")
-for number, line in enumerate (file):
-    word_list = split_line(line)
-    for word in word_list:
 
-        lower_bound = 0
-        upper_bound = len(dictionary_list) - 1
-        found = False
+# Reading AliceInWonderLand200.txt
+file = open("AliceInWonderLand200.txt")
+for number, line in enumerate (file):  # for loop to iterate through
+    # each line of story and numerate each line assigning it to variable i
+    word_list = split_line(line) # splitting apart the line of text
+    # in the story and assigning variable word_list
+    for word in word_list: # for loop to iterate through each word in
+        # the words list
+
+        lower_bound = 0  # Low Bound of binary search
+        upper_bound = len(dictionary_list) - 1 # Upper bound of binary search
+        found = False  # indicator that shows if the word is found or not
 
         # Loop until we find the item, or our upper/lower bounds meet
         while lower_bound <= upper_bound and not found:
 
             # Find the middle position
             middle_pos = (lower_bound + upper_bound) // 2
-
+            # Figure out if we:
+            # move up the lower bound, or
+            # move down the upper bound, or
+            # we found what we are looking for
             if dictionary_list[middle_pos] < word.upper():
                 lower_bound = middle_pos + 1
             elif dictionary_list[middle_pos] > word.upper():
@@ -59,7 +71,7 @@ for number, line in enumerate (file):
             else:
                 found = True
 
-        if not found:
+        if not found: # If the word is not found
             print(f"Line {number + 1}, possible misspelled word:{word}")
 
-file.close()
+file.close()  # Closing the AliceInWonderLand200.txt
